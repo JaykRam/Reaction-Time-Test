f408ef10f537f3b07d5ecd2cbc32e545671ad1d4
Bob <jaykmalhbergramolete@gmail.com>
2025-03-25 23:31:52 +0800

    Added functionality. On click, start game and send to Red Screen.

--- React.py
+++ React.py
@@ -22,22 +22,40 @@ import tkinter as tk
 # Close
 # startscreen.destroy()
 
+
+
+# Initialize Screen
+screen = tk.Tk()
+
 def start_react():
-    startscreen = tk.Tk()
+    startscreen = screen
     startscreen.title("Start Screen")
 
     startscreen.geometry("400x300")
     startscreen.configure(bg="sky blue")
 
-    def on_blue_click(event):
-        print("Screen clicked")
-        
-    
     startscreen.bind("<Button-1>", on_blue_click)
+    
 
-    print("React game started")
-    startscreen.mainloop()
+def red_screen():
+    readyscreen = screen
+    readyscreen.title("Get Ready Screen")
+
+    readyscreen.geometry("400x300")
+    readyscreen.configure(bg="red")
+
+    #add random time before switch to green
 
 
 
+
+
+# Start React Game. Go to red screen.
+def on_blue_click(event):
+    print("Screen clicked")
+    red_screen()
+
+# Debug and Start the game.
 start_react()
+print("React game started")
+screen.mainloop()
